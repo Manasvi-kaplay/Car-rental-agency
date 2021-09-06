6d0eb0d7f5614d5b7af4fa76328e8259d1af9b84
Manasvi Kaplay <manasvi111.kaplay@gmail.com>
2021-09-06 11:59:11 +0530

    6th sept

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
 var session=require("express-session");
+//var cookie=require("cookie-parser");
 var ejs=require("ejs");
+var cookieParser = require("cookie-parser");
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
   extended:false,
@@ -15,6 +17,7 @@ app.use(bodyParser.json({
   limit: '50mb'
 }));
 app.use(express.static(__dirname+"/public"));
+app.use(cookieParser())
 app.use(session({ secret : "TSS", saveUninitialized: true}));
 app.use(require("./controller/default"));
 app.listen(process.env.PORT || 1500,function(){
--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
 var mysql=require("mysql");
 //establishing a connection with mysql
 module.exports=mysql.createPool({
-    host: 'db4free.net',
+    /*host: 'db4free.net',
         port:3306,
         user: 'manasvi123',
-        password: '0e06bbf0',
-        database: 'car_rental_sys'
+        password: '0e06bbf0',*/
+        host:'localhost',
+        port:3306,
+        user:'root',
+        password:'',
+        database: 'car_rental_agency'
 })
 //module.exports=options;
